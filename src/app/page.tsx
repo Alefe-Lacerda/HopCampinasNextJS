38133706803a634405ee4b018b9bbb999ff733bd
Alefe-Lacerda <alefeguimel12@gmail.com>
2026-05-24 12:44:32 -0300

    redirect to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
 'use client';
 
-import Link from "next/link";
-import { useRouter } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleNavigation = () => {
-    router.push('/home-page');
-  };
-
   return (
-    <>
-      <button onClick={handleNavigation} className="hover:underline hover: cursor-pointer">
-        Ir para Home via Código
-      </button>
-    </>
+    redirect("/home-page")
   );
 
 }
