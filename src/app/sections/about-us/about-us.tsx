46b74505514acf75e09361d9fcd66d36ab6e07e6
Lucas Borges <lucasborges@192.168.15.45>
2026-06-03 22:36:47 -0300

    feat: improvements on home page and create about us section

--- src/app/sections/about-us/about-us.tsx
+++ src/app/sections/about-us/about-us.tsx
@@ -3,30 +3,38 @@ import styles from "./about-us.module.css";
 
 const AboutUs = () => {
   return (
-    <section className={styles.container}>
-      <span className={styles.title}>nossa missão</span>
-      <h1>
-        Harpa & Taça -
-        <br />
-        Oração e adoração
-      </h1>
-      <span>
-        Como João Batista (Lucas 1:76-79) e a profetiza Ana (Lucas 2:36-38), que eram precursores de
-        Jesus Cristo na Terra, o primeiro preparando o caminho com a palavra revelada no deserto e a
-        segunda estabelecendo um trono ao Salvador com orações e consagração dia e noite - nossa
-        missão é preparar o caminho para a volta de Jesus.
-      </span>
-      <div className={styles.cardboard}>
-        <div className={styles.verticalLine} />
-        <span>
-          "Estabelecer a oração e adoração dia e noite, a fim de conectar nossos corações ao coração
-          do Noivo."
+    <section id="sobre-nos" className={styles.container}>
+      <div className={styles.leftContent}>
+        <span className={styles.title}>nossa missão</span>
+        <h1>
+          Harpa & Taça -
+          <br />
+          Oração e adoração
+        </h1>
+        <span className={styles.description}>
+          Como João Batista (Lucas 1:76-79) e a profetiza Ana (Lucas 2:36-38), que eram precursores
+          de Jesus Cristo na Terra, o primeiro preparando o caminho com a palavra revelada no
+          deserto e a segunda estabelecendo um trono ao Salvador com orações e consagração dia e
+          noite - nossa missão é preparar o caminho para a volta de Jesus.
         </span>
-        <p>modelo harpa & taça</p>
+        <div className={styles.cardboard}>
+          <div className={styles.verticalLine} />
+          <div className={styles.cardContent}>
+            <span>
+              "Estabelecer a oração e adoração dia e noite, a fim de conectar nossos corações ao
+              coração do Noivo."
+            </span>
+            <p>modelo harpa & taça</p>
+          </div>
+        </div>
       </div>
-      <div className={styles.imageContainer}>
-        <Image src={""} alt="" />
-      </div>
+      <Image
+        className={styles.image}
+        src={"/images/calice.jpg"}
+        alt="Harpa e Taça"
+        width={430}
+        height={580}
+      />
     </section>
   );
 };
